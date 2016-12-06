ce85224ae7f482c0fae4a982e5dd3cdd3ea46739
Andrea Oliva <aoliva71@gmail.com>
2016-12-06 00:05:24 +0000

    writing pidfile

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 
 #include <getopt.h>
+#include <limits.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,7 @@
 struct _conf
 {
     int daemonize;
+    char pidfile[PATH_MAX + 1];
 };
 typedef struct _conf conf_t;
 
@@ -17,6 +19,7 @@ static void killer(int);
 
 static void set_defaults(conf_t *);
 static void parse_command_line(conf_t *, int, char *[]);
+static void write_pid(conf_t *);
 
 int main(int argc, char *argv[])
 {
@@ -30,6 +33,8 @@ int main(int argc, char *argv[])
     }
 
     signal(SIGINT, killer);
+
+    write_pid(&conf);
 
     for(keep_running = 1; keep_running;) {
         /* check remote changes */
@@ -54,6 +59,7 @@ static void killer(int s)
 static void set_defaults(conf_t *conf)
 {
     memset(conf, 0, sizeof(conf_t));
+    strncpy(conf->pidfile, "/var/run/gdd.pid", PATH_MAX);
 }
 
 static void parse_command_line(conf_t *conf, int argc, char *argv[])
@@ -62,6 +68,7 @@ static void parse_command_line(conf_t *conf, int argc, char *argv[])
 #define OPTS    "dh"
     static struct option lopts[] = {
         {"daemonize", 0, NULL, 'd'},
+        {"pid-file", 1, NULL, 'p'},
         {"help", 0, NULL, 'h'},
         {0, 0, 0, 0}
     };
@@ -75,9 +82,15 @@ static void parse_command_line(conf_t *conf, int argc, char *argv[])
         case 'd':
             conf->daemonize = 1;
             break;
+        case 'p':
+            if(optarg) {
+                strncpy(conf->pidfile, optarg, PATH_MAX);
+            }
+            break;
         case 'h':
             printf("usage: %s "
-                "-d|--daemonize "
+                "[-d|--daemonize] "
+                "[-p|--pid-file <PIDFILE>] "
                 " | "
                 "-h|--help\n", argv[0]);
             exit(0);
@@ -85,4 +98,16 @@ static void parse_command_line(conf_t *conf, int argc, char *argv[])
     }
 }
 
+static void write_pid(conf_t *conf)
+{
+    pid_t pid;
+    FILE *f;
+
+    pid = getpid();
+    f = fopen(conf->pidfile, "w");
+    if(f) {
+        fprintf(f, "%d", (int)pid);
+        fclose(f);
+    }
+}
 
